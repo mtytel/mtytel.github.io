cb2e143e3a581c60f06fd459739130c848eb5dcf
Matthew <matthewtytel@gmail.com>
2015-07-07 17:04:49 -0700

    Publishing from jekyll...

--- static/js/helm_download.js
+++ static/js/helm_download.js
@@ -11,8 +11,8 @@ $(document).ready(function initDownload() {
   else if (navigator.appVersion.indexOf("X11") != -1 ||
            navigator.appVersion.indexOf("Linux") != -1) {
     if (navigator.oscpu.indexOf("64") != -1)
-      download_link.prop("href", "https://launchpad.net/~tytel/+archive/ubuntu/helm/+build/7629771/+files/helm_0.3-1_amd64.deb");
+      download_link.prop("href", "https://launchpad.net/~tytel/+archive/ubuntu/helm/+build/7630257/+files/helm_0.3-2_amd64.deb");
     else
-      download_link.prop("href", "https://launchpad.net/~tytel/+archive/ubuntu/helm/+build/7629772/+files/helm_0.3-1_i386.deb");
+      download_link.prop("href", "https://launchpad.net/~tytel/+archive/ubuntu/helm/+build/7630258/+files/helm_0.3-2_i386.deb");
   }
 });
