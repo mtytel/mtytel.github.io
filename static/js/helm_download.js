f4f4c8915d23aaea8301005510cc1b057a6ee08b
Matthew <matthewtytel@gmail.com>
2015-08-03 19:48:19 -0700

    Publishing from jekyll...

--- static/js/helm_download.js
+++ static/js/helm_download.js
@@ -1,18 +1,18 @@
 $(document).ready(function initDownload() {
   var download_link = $("#download-link");
   if (navigator.appVersion.indexOf("Mac") != -1)
-    download_link.prop("href", "/static/dist/Helm_v0_4_0.pkg");
+    download_link.prop("href", "/static/dist/Helm_v0_4_1.pkg");
   else if (navigator.appVersion.indexOf("Win") != -1) {
     if (navigator.oscpu.indexOf("64") != -1)
-      download_link.prop("href", "/static/dist/Helm_64bit_v0_4_0.msi");
+      download_link.prop("href", "/static/dist/Helm_64bit_v0_4_1.msi");
     else
-      download_link.prop("href", "/static/dist/Helm_32bit_v0_4_0.msi");
+      download_link.prop("href", "/static/dist/Helm_32bit_v0_4_1.msi");
   }
   else if (navigator.appVersion.indexOf("X11") != -1 ||
            navigator.appVersion.indexOf("Linux") != -1) {
     if (navigator.oscpu.indexOf("64") != -1)
-      download_link.prop("href", "https://launchpad.net/~tytel/+archive/ubuntu/helm/+build/7652634/+files/helm_0.4.0-1_amd64.deb");
+      download_link.prop("href", "https://code.launchpad.net/~tytel/+archive/ubuntu/helm/+build/7737621/+files/helm_0.4.1-0%7E699%7Eubuntu14.04.1_amd64.deb");
     else
-      download_link.prop("href", "https://launchpad.net/~tytel/+archive/ubuntu/helm/+build/7652635/+files/helm_0.4.0-1_i386.deb");
+      download_link.prop("href", "https://code.launchpad.net/~tytel/+archive/ubuntu/helm/+build/7737622/+files/helm_0.4.1-0%7E699%7Eubuntu14.04.1_i386.deb");
   }
 });
