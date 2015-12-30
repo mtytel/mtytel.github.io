90da1ae1467146b68be078f8b33fa519c0152aae
mtytel <matthewtytel@gmail.com>
2015-12-29 20:48:49 -0500

    Publishing from jekyll...

--- static/js/helm_donation_slider.js
+++ static/js/helm_donation_slider.js
@@ -1,4 +1,3 @@
-
 /**
  *
  * @licstart  The following is the entire license notice for the
@@ -26,43 +25,110 @@
  *
  */
 
-$(function() {
-  var default_amount_index = 3;
-  var amounts = ["Nope", "$1", "$2", "$5", "$10", "$20", "$50"];
+function showSticker() {
+  var logo = $('#helm-logo');
+  logo.fadeOut(400, function() {
+    logo.prop("src", "/static/images/helm_sticker_small.png");
+    $(this).fadeIn(400);
+  });
+}
+
+function showDonate() {
+  var downloads = $("#downloads");
+  var slider_section = $("#slider-section");
+  downloads.fadeTo(400, 0, function() {
+    downloads.css({"visibility":"hidden"});
+
+    slider_section.css({"visibility":"visible"});
+    slider_section.fadeTo(400, 1);
+  });
+
+  var catch_phrase = $("#catch-phrase");
+  catch_phrase.fadeOut(400, function() {
+    catch_phrase.text("what you want to");
+    $(this).fadeIn(400);
+  });
+
+  var name = $("#product-name");
+  name.fadeOut(400, function() {
+    name.text("Pay");
+    $(this).fadeIn(400);
+  });
+}
+
+function setupDonateSlider() {
+  var default_amount_index = 4;
+  var amounts = [0, 1, 2, 5, 10, 20, 50, 100];
+  var amounts_display = ["$-"];
+  for (var i = 1; i < amounts.length; i++)
+    amounts_display.push("$" + amounts[i]);
+
   var amount = $("#donation-amount");
-  amount.val(amounts[default_amount_index]);
+  amount.val(amounts_display[default_amount_index]);
   var donation = $("#donation");
 
   var slider_section = $("#slider-section");
-  var downloads = $("#downloads");
 
   slider_section.css({
     "position":"absolute",
   });
+
   slider_section.fadeTo(0, 0);
   slider_section.hide();
 
-  var download_link = $("#download-link");
-  download_link.click(function() {
-    downloads.fadeTo("slow", 0, function() { downloads.hide(); });
-    slider_section.fadeTo("slow", 1);
-    slider_section.css({
-      "visibility":"visible",
-    });
-  });
-
-  var downloads = $("#downloads");
-
   var slider = $("<div id='slider'></div>").insertAfter(donation).slider({
     min: 0,
-    max: 6,
+    max: 7,
     range: "min",
     value: default_amount_index,
     slide: function(event, ui) {
-      amount.val(amounts[ui.value]);
+      amount.val(amounts_display[ui.value]);
+      var contribute_button = $("#contribute-button");
+      var nopay_button = $("#nopay-button");
+      if (ui.value == 0) {
+        nopay_button.show();
+        contribute_button.hide();
+      }
+      else {
+        nopay_button.hide();
+        contribute_button.show();
+      }
     }
   });
-  $("#donation-amount").change(function() {
-    slider.slider("value", this.val());
+
+  amount.on("input", function() {
+    var value = $(this).val();
+    value = value.replace("$", "");
+    var float_value = parseFloat(value);
+    var contribute_button = $("#contribute-button");
+    var nopay_button = $("#nopay-button");
+
+    if (float_value > 0) {
+      var slider_index = 1;
+      for (var i = 1; i < amounts.length - 1; ++i) {
+        if (float_value > amounts[i])
+          slider_index = i + 1;
+      }
+
+      slider.slider("option", "value", slider_index);
+      nopay_button.hide();
+      contribute_button.show();
+    }
+    else {
+      slider.slider("option", "value", 0);
+      nopay_button.show();
+      contribute_button.hide();
+    }
+  });
+}
+
+$(function() {
+  setupDonateSlider();
+
+  var download_link = $("#download-link");
+  download_link.click(function() {
+    ga("send", "event", "link", "click", download_link.prop("href"));
+    showDonate();
+    return false;
   });
 });
--- static/js/helm_tooltip.js
+++ static/js/helm_tooltip.js
@@ -1,4 +1,3 @@
-
 /**
  *
  * @licstart  The following is the entire license notice for the
@@ -26,40 +25,15 @@
  *
  */
 
-function getOsDownload() {
-  var linux32 = "/static/dist/helm_0.6.0-863~ubuntu14.04.1_i386.deb";
-  var linux64 = "/static/dist/helm_0.6.0-863~ubuntu14.04.1_amd64.deb";
-  var osx = "/static/dist/Helm_v0_6_0.pkg";
-  var win32 = "/static/dist/Helm_32bit_v0_6_0.msi";
-  var win64 = "/static/dist/Helm_64bit_v0_6_0.msi";
+$(document).ready(function() {
+  $('#sticker-suggest').tooltipster({
+    content: $('<span><img id="sticker-tooltip" src="/static/images/helm_sticker.png" /></span>'),
+    position: 'bottom',
+  });
 
-  if (navigator.appVersion.indexOf("Mac") != -1)
-    return osx;
-
-  var os_string = null;
-  if (navigator.userAgent)
-    os_string = navigator.userAgent;
-  else if (navigator.oscpu)
-    os_string = navigator.oscpu;
-  else if (navigator.platform)
-    os_string = navigator.platform;
-
-  if (os_string == null)
-    return null;
-
-  if (navigator.appVersion.indexOf("Win") != -1) {
-    if (os_string.indexOf("64") != -1)
-      return win64;
-    else
-      return win32;
-  }
-  else if (navigator.appVersion.indexOf("X11") != -1 ||
-           navigator.appVersion.indexOf("Linux") != -1) {
-    if (os_string.indexOf("64") != -1)
-      return linux64;
-    else
-      return linux32;
-  }
-
-  return null;
-};
+  $('.sticker-order').tooltipster({
+    content: $('<span><img id="sticker-tooltip" src="/static/images/helm_sticker.png" /></span>'),
+    position: 'left',
+    offsetY: 110,
+  });
+});
